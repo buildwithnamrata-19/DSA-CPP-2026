cb274273ee8e26dab1cb9b5524f99a65b4b3878a
buildwithnamrata-19 <buildwithnamrata@gmail.com>
2026-05-29 11:40:47 +0530

    Day 4: solve Search Insert Position using Binary Search

---
+++ Arrays/SearchInsertPosition.cpp
@@ -0,0 +1,38 @@
+/*
+Problem:
+LeetCode 35 - Search Insert Position
+
+Approach:
+Use Binary Search to find target or insertion position.
+
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            else if(nums[mid] < target)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
+        }
+
+        return left;
+    }
+};
