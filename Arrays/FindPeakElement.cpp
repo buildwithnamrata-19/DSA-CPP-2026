3500f17585c6b0797667afd6946411f06cd79c5b
buildwithnamrata-19 <buildwithnamrata@gmail.com>
2026-05-29 11:42:46 +0530

    Day 4: solve Find Peak Element using Binary Search

---
+++ Arrays/FindPeakElement.cpp
@@ -0,0 +1,31 @@
+/*
+Problem:
+LeetCode 162 - Find Peak Element
+
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] > nums[mid + 1])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+};
