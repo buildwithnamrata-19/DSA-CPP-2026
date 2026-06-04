3196c9cba2c8afcf22d11488333ec91ab7b76d1c
buildwithnamrata-19 <buildwithnamrata@gmail.com>
2026-06-04 19:18:51 +0530

    Day 10: solve Valid Parentheses using Stack

---
+++ Strings/ValidParentheses.cpp
@@ -0,0 +1,43 @@
+/*
+Problem:
+LeetCode 20 - Valid Parentheses
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        for(char c : s) {
+
+            if(c=='(' || c=='{' || c=='[')
+                st.push(c);
+
+            else {
+
+                if(st.empty())
+                    return false;
+
+                if(c==')' && st.top()!='(')
+                    return false;
+
+                if(c=='}' && st.top()!='{')
+                    return false;
+
+                if(c==']' && st.top()!='[')
+                    return false;
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
