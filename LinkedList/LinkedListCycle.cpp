3a48ba0c5c27b43e20755e5a9fb4a27de02491cc
buildwithnamrata-19 <buildwithnamrata@gmail.com>
2026-06-08 00:04:02 +0530

    Day 13: solve Linked List Cycle using Fast and Slow Pointers

---
+++ LinkedList/LinkedListCycle.cpp
@@ -0,0 +1,27 @@
+/*
+Problem:
+LeetCode 141 - Linked List Cycle
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next) {
+
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
