423669bfffe867920403eb8e6c4d8f7748a53573
buildwithnamrata-19 <buildwithnamrata@gmail.com>
2026-06-08 00:03:25 +0530

    Day 13: solve Merge Two Sorted Lists

---
+++ LinkedList/MergeTwoSortedLists.cpp
@@ -0,0 +1,39 @@
+/*
+Problem:
+LeetCode 21 - Merge Two Sorted Lists
+
+Time Complexity: O(n+m)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(
+        ListNode* list1,
+        ListNode* list2) {
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while(list1 && list2) {
+
+            if(list1->val < list2->val) {
+
+                tail->next = list1;
+                list1 = list1->next;
+            }
+            else {
+
+                tail->next = list2;
+                list2 = list2->next;
+            }
+
+            tail = tail->next;
+        }
+
+        tail->next =
+            list1 ? list1 : list2;
+
+        return dummy.next;
+    }
+};
