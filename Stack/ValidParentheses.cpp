ae1a5aa1ac615f666d0f0e04b0dd3f430beae156
buildwithnamrata-19 <buildwithnamrata@gmail.com>
2026-06-12 10:00:08 +0530

    Day 18: solve Valid Parentheses using Stack

---
+++ Stack/ValidParentheses.cpp
@@ -0,0 +1,39 @@
+/*
+LeetCode 20
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        for(char c : s) {
+
+            if(c=='(' || c=='{' || c=='[')
+                st.push(c);
+
+            else {
+
+                if(st.empty())
+                    return false;
+
+                if(c==')' && st.top()!='(')
+                    return false;
+
+                if(c=='}' && st.top()!='{')
+                    return false;
+
+                if(c==']' && st.top()!='[')
+                    return false;
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
