0c63a3087c7e894dd53e46404ac81fb4e14f6b33
buildwithnamrata-19 <buildwithnamrata@gmail.com>
2026-06-13 19:48:38 +0530

    Day 19: solve Car Fleet using Stack and Greedy

---
+++ Stack/CarFleet.cpp
@@ -0,0 +1,53 @@
+/*
+Problem:
+LeetCode 853 - Car Fleet
+
+Approach:
+Sorting + Stack
+
+Time Complexity: O(n log n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    int carFleet(
+        int target,
+        vector<int>& position,
+        vector<int>& speed) {
+
+        vector<pair<int,int>> cars;
+
+        for(int i = 0;
+            i < position.size();
+            i++) {
+
+            cars.push_back(
+                {position[i], speed[i]}
+            );
+        }
+
+        sort(cars.begin(),
+             cars.end());
+
+        stack<double> st;
+
+        for(int i = cars.size()-1;
+            i >= 0;
+            i--) {
+
+            double time =
+                (double)(target -
+                cars[i].first)
+                / cars[i].second;
+
+            if(st.empty() ||
+               time > st.top()) {
+
+                st.push(time);
+            }
+        }
+
+        return st.size();
+    }
+};
