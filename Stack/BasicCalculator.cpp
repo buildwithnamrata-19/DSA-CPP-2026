951ba2b6b5bc3e758c4d0d606d4fe2e39f8521b7
buildwithnamrata-19 <buildwithnamrata@gmail.com>
2026-06-16 23:30:25 +0530

    Day 22: solve Basic Calculator using Stack

---
+++ Stack/BasicCalculator.cpp
@@ -0,0 +1,65 @@
+/*
+LeetCode 224
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    int calculate(string s) {
+
+        stack<int> st;
+
+        int result = 0;
+        int number = 0;
+        int sign = 1;
+
+        for(char c : s) {
+
+            if(isdigit(c)) {
+
+                number =
+                    number * 10 +
+                    (c - '0');
+            }
+
+            else if(c == '+') {
+
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            }
+
+            else if(c == '-') {
+
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            }
+
+            else if(c == '(') {
+
+                st.push(result);
+                st.push(sign);
+
+                result = 0;
+                sign = 1;
+            }
+
+            else if(c == ')') {
+
+                result += sign * number;
+                number = 0;
+
+                result *= st.top();
+                st.pop();
+
+                result += st.top();
+                st.pop();
+            }
+        }
+
+        return result +
+               sign * number;
+    }
+};
