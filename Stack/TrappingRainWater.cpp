17144df2f5e2f418e11cdd47e7291378ee4ccddd
buildwithnamrata-19 <buildwithnamrata@gmail.com>
2026-06-17 21:12:29 +0530

    Day 23: solve Trapping Rain Water using Monotonic Stack

---
+++ Stack/TrappingRainWater.cpp
@@ -0,0 +1,44 @@
+/*
+LeetCode 42
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+        stack<int> st;
+        int water = 0;
+
+        for(int i = 0; i < height.size(); i++) {
+
+            while(!st.empty() &&
+                  height[i] > height[st.top()]) {
+
+                int top = st.top();
+                st.pop();
+
+                if(st.empty())
+                    break;
+
+                int distance =
+                    i - st.top() - 1;
+
+                int boundedHeight =
+                    min(height[i],
+                    height[st.top()])
+                    - height[top];
+
+                water +=
+                    distance *
+                    boundedHeight;
+            }
+
+            st.push(i);
+        }
+
+        return water;
+    }
+};
