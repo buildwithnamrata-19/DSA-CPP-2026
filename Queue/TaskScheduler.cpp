58fb503c131fa42c8ef71c885015c318dcd4831d
buildwithnamrata-19 <buildwithnamrata@gmail.com>
2026-06-19 20:07:38 +0530

    Day 25: solve Task Scheduler using Queue and Greedy

---
+++ Queue/TaskScheduler.cpp
@@ -0,0 +1,39 @@
+/*
+LeetCode 621
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int leastInterval(
+        vector<char>& tasks,
+        int n) {
+
+        vector<int> freq(26, 0);
+
+        for(char task : tasks)
+            freq[task - 'A']++;
+
+        int maxFreq =
+            *max_element(
+                freq.begin(),
+                freq.end());
+
+        int maxCount = 0;
+
+        for(int f : freq) {
+
+            if(f == maxFreq)
+                maxCount++;
+        }
+
+        return max(
+            (int)tasks.size(),
+            (maxFreq - 1) *
+            (n + 1) +
+            maxCount
+        );
+    }
+};
