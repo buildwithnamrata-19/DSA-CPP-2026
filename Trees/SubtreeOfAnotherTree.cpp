d69733b8b0770f910d2761635c16258f6b7eb1c0
buildwithnamrata-19 <buildwithnamrata@gmail.com>
2026-06-24 10:13:45 +0530

    Day 30: solve Subtree Of Another Tree

---
+++ Trees/SubtreeOfAnotherTree.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    bool same(
+        TreeNode* p,
+        TreeNode* q) {
+
+        if(!p && !q)
+            return true;
+
+        if(!p || !q)
+            return false;
+
+        if(p->val != q->val)
+            return false;
+
+        return same(
+                   p->left,
+                   q->left
+               ) &&
+               same(
+                   p->right,
+                   q->right
+               );
+    }
+
+    bool isSubtree(
+        TreeNode* root,
+        TreeNode* subRoot) {
+
+        if(root == nullptr)
+            return false;
+
+        if(same(root, subRoot))
+            return true;
+
+        return isSubtree(
+                   root->left,
+                   subRoot
+               ) ||
+               isSubtree(
+                   root->right,
+                   subRoot
+               );
+    }
+};
